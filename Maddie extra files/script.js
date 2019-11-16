86ba45c270e44946779caa9ffd41237a3fa48a60
Nanaem808 <nanaem808@Nanaem808s-MacBook-Pro.local>
2019-11-16 11:58:10 -0600

    new links in foundation file. it is flexbox and tutorial inside

---
+++ Maddie extra files/script.js
@@ -0,0 +1,17 @@
+<script>
+  $(document).ready(function(){
+
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  })
+})
+});
+
+
+</script>
