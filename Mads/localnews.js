b0aa3212fc51e8cfbeb15c34fd3847f5927bcd45
Nanaem808 <nanaem808@Nanaem808s-MBP.lan1>
2019-11-22 10:39:20 -0600

    getting hangman to work at the bottom of the page on page

--- Mads/localnews.js
+++ Mads/localnews.js
@@ -1,49 +1,19 @@
 document.ready(function () {
 
-    var newsTitles = ["titleA", "titleB", "titleC", "titleD", "titleE", "titleF", "titleG", "titleH", "titleI"];
-    var investTitles = ["titleJ", "titleK", "titleL", "titleM", "titleN", "titleO", "titleP", "titleQ"];
-    var newsSubs = ["subA", "subB", "subC", "subD", "subE", "subF", "subG", "subH", "subI"];
-    var investSubs = ["subJ", "subK", "subL", "subM", "subN", "subO", "subP", "subQ"];
-    var newsPics = ["picA", "picB", "picC", "picD", "picE", "picF", "picG", "picH", "picI"];
-    var investPics = ["picJ", "picK", "picL", "picM", "picN", "picO", "picP", "picQ"];
 
-    //Creating the URLs for business and finance sections
-    var busURL = "https://newsapi.org/v2/everything?apiKey=" + apiKey + "&" + catBusiness + "&" + from + "&" + to + "&" + finDomains + "&" + sortBy;
-    var investURL = "https://newsapi.org/v2/everything?apiKey=" + apiKey + "&" + catInvest + "&" + from + "&" + to + "&" + investDomains + "&" + sortBy;
-
-
-
-    function newsQuery(queryURL) {
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (newsData) {
-            for (var i = 0; i < newsTitles.length; i++) {
-                $(`.${newsTitles[i]}`).text(newsData.articles[i].title);
-                $(`.${newsSubs[i]}`).text(newsData.articles[i].description);
-                $(`.${newsPics[i]}`).attr('src', newsData.articles[i].urlToImage);
-                $(`.${newsPics[i]}`).wrap(`<a class="picLink" href="${newsData.articles[i].url}"></a>`);
-                $(`.${newsTitles[i]}`).wrap(`<a class="titleLink" href="${newsData.articles[i].url}"></a>`);
+    var i = 0;
+    function adverts() {
+        interval = setInterval(function () {
+            var imagesA = ['../assets/Images/tdAmeritrade.jpg', '../assets/Images/invesco.jpg', '../assets/Images/fidelity.png'];
+            var imagesB = ['../assets/Images/ally.jpg', '../assets/Images/marketWatch.png', '../assets/Images/wells-fargo.jpg'];
+            // var i = 0;
+            $('.adv-pic-a').attr('src', imagesA[i]);
+            $('.adv-pic-b').attr('src', imagesB[i]);
+            i++;
+            if (i > imagesA.length) {
+                i = 0
             }
-        })
+        }, 4000);
     }
-    newsQuery(busURL);
-
-    //This function is for querying the investing API and grabing the information needed to populate the HTML
-    function investQuery(queryURL) {
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (invData) {
-            for (var i = 0; i < investTitles.length; i++) {
-                $(`.${investTitles[i]}`).text(invData.articles[i].title);
-                $(`.${investSubs[i]}`).text(invData.articles[i].description);
-                $(`.${investPics[i]}`).attr('src', invData.articles[i].urlToImage);
-                $(`.${investPics[i]}`).wrap(`<a class="picLink" href="${invData.articles[i].url}"></a>`);
-                $(`.${investTitles[i]}`).wrap(`<a class="titleLink" href="${invData.articles[i].url}"></a>`);
-            }
-        })
-    }
-    investQuery(investURL);
-
+    adverts();
 });
