053b73dec881e1fc8eaefb53edb3b6625c66edef
Matthew Knight <mattknight.sa@gmail.com>
2019-11-21 19:56:29 -0600

    update with hangman

--- mattsfolder/js/app.js
+++ mattsfolder/js/app.js
@@ -1,9 +1,8 @@
 var quarterBack = ["t","o","m","b","r","a","d","y"];
-
 var incorrectAnswer = ["h","a","n","g","m","a","n"]; 
 var userArray = [];
 var hangman = "";
-var answer = "";
+var answer = [];
 var i = 0;
 var name;
 var email;
@@ -14,71 +13,36 @@ var email;
     response = response.toLowerCase();
     if(quarterBack.includes(response)) {
         $("." + response).text(response);
-    }else {
+        answer.push(response);
+        if(answer.length == quarterBack.length) {
+        $('.congrats').css('display', 'inline');
+        $('.sweepstakes').css('display', 'inline');
+        $('h3').css('display', 'none');
+        }
+     }else {
         hangman += (incorrectAnswer[i]);
+        $('h3').append(incorrectAnswer[i]);
         i++
     }
-     if(hangman == "hangman") {
-        alert("Game Over!");
+    if(hangman == "hangman") {
+        $('.over').css('display', 'inline');
         $('.sweepstakes').css('display', 'inline');
     }
     console.log(hangman)
     $('#answer').val("");
-
 })
-
 $('.button').click(function(event) {
     event.preventDefault();
 name = $('.name').val();
 email = $('.email').val();
 console.log(name)
 console.log(email)
-
 newUser = {
     person: name,
     email: email
   }
-
   userArray.push(newUser);
   userArray = userArray.concat(JSON.parse(localStorage.getItem('users') || '[]'));
   window.localStorage.setItem('users', JSON.stringify(userArray));
-
-
-
 })
 
-// newUser = {
-//     person: name,
-//     email: email
-//   }
-
-//   cityArray.push(newUser);
-//   userArray = userArray.concat(JSON.parse(localStorage.getItem('users') || '[]'));
-//   window.localStorage.setItem('users', JSON.stringify(userArray));
-
-// function algo(x) {
-//     if(x == "t"){
-//         $(".t").text("T");
-//     }
-//     if(x == "o"){
-//         $(".o").text("o");
-//     }
-//     if(x == "m"){
-//         $(".m").text("m");
-//     }
-//     if(x == "b"){
-//         $(".b").text("B");
-//     }
-//     if(x == "r"){
-//         $(".r").text("r");
-//     }
-//     if(x == "a"){
-//         $(".a").text("a");
-//     }
-//     if(x == "d"){
-//         $(".d").text("d");
-//     }
-//     if(x == "y"){
-//         $(".y").text("y");
-//     }
-// }
--- mattsfolder/js/script.js
+++ mattsfolder/js/script.js
@@ -4,10 +4,12 @@ var main = "Lamar Jackson";
 var subArticles = ["LSU", "Lebron James", "New England Patriots", "Houston Astros"];
 var images = ["picB", "picC", "picD", "picE"];
 var queryURL = "https://newsapi.org/v2/everything?q=" + main +"&apiKey=" + newsApi;
+queryURL = "https://newsapi.org/v2/everything?q=" + "Lamar Jackson" + "&apiKey=c6d47e8264e749ff85f4e6cc2155d4e6";
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
+    console.log(queryURL)
     $('.titleA').text(response.articles[2].title);
     $('.subA').text(response.articles[2].description);
     $('.picA').attr('src', response.articles[2].urlToImage);
@@ -24,8 +26,9 @@ $.ajax({
     
 })
 
-i = 0;
+// i = 0;
 function adverts () {
+    i = 1;
     interval = setInterval(function(){
     var images = ['images/target.jpeg', 'images/dickssportinggoods.jpeg', 'images/whataburger.jpeg'];
     // var i = 0;
@@ -34,7 +37,7 @@ function adverts () {
     if(i > images.length) {
     i = 0
     }
-    },3000);
+    },1000);
 }
 
 
