7521e65242c0cb617f15432f59e82a6a873f47cd
madeline moritz <mmoritz@stedwards.edu>
2019-11-23 09:03:53 -0600

    i've hit a wall

--- Mads/localnews.js
+++ Mads/localnews.js
@@ -9,6 +9,33 @@ var results = response.data;
 var targetOne = document.getElementById("target2");
 $('#target2').empty();
 $("#target2").text(response.articles[0].description + response.articles[0].title);
+}).then(function (response) {
+                    console.log(this);
+                    console.log(response.data);
+                    var results = response.data;
+                    var targetOne = document.getElementById("target");
+                    $('#target').empty();
+                    $('#target1').empty();
+
+                    $("#target").text(JSON.stringify(response));
+
+                    var len = response.length;
+                    var spot = "";
+                    for (var i = 0; i < len; i++) {
+
+                         var title = response.article[0].title;
+                         var article = response.article[0].article;
+                         var description = response.article[0].description;
+                         var source = response.article[0].source;
+                         var author = response.author;
+                         var publish = response.article[0].publishedAt;
+                         var urltoImage = response.article[0].urlToImage;
+
+                         spot.text = title + article + description + source + author + publish + urltoImage;
+                         spot.toString();
+                         $('#target1').heml(spot);
+                    }
+               })
 
 
 });
