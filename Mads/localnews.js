b2856ed14ba24d4d8190642309222b9f3a604396
madeline moritz <mmoritz@stedwards.edu>
2019-11-23 08:48:17 -0600

    almost done

--- Mads/localnews.js
+++ Mads/localnews.js
@@ -1,19 +1,14 @@
-document.ready(function () {
+$.ajax({
+url: 'https://newsapi.org/v2/everything?sortBy=relevancy&q=austin+texas+local+news&pageSize=2&apiKey=d9da22da0a2347a58fea3976369ee1f1',
+method: "GET",
+
+}).then(function (response) {
+console.log(this);
+console.log(response.data);
+var results = response.data;
+var targetOne = document.getElementById("target2");
+$('#target2').empty();
+$("#target2").text(response.articles[0].description + response.articles[0].title);
 
 
-    var i = 0;
-    function adverts() {
-        interval = setInterval(function () {
-            var imagesA = ['../assets/Images/tdAmeritrade.jpg', '../assets/Images/invesco.jpg', '../assets/Images/fidelity.png'];
-            var imagesB = ['../assets/Images/ally.jpg', '../assets/Images/marketWatch.png', '../assets/Images/wells-fargo.jpg'];
-            // var i = 0;
-            $('.adv-pic-a').attr('src', imagesA[i]);
-            $('.adv-pic-b').attr('src', imagesB[i]);
-            i++;
-            if (i > imagesA.length) {
-                i = 0
-            }
-        }, 4000);
-    }
-    adverts();
 });
