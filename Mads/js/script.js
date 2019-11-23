59114281341f7430109fa8a570ef0353bffb58b8
madeline moritz <mmoritz@stedwards.edu>
2019-11-23 06:33:56 -0600

    moving stuff around

--- Mads/js/script.js
+++ Mads/js/script.js
@@ -1,69 +1,19 @@
-var a1 = $.ajax({}),
-	a2 = $.ajax({}),
-	a3 = $.ajax({}),
-	a4 = $.ajax({}),
-	a5 = $.ajax({}),
-	a6 = $.ajax({}),
-	a7 = $.ajax({}),
-	a8 = $.ajax({}),
-	a9 = $.ajax({}),
-	a10 = $.ajax({}),
-	a11 = $.ajax({}),
-	a12 = $.ajax({});
-
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "queryURL",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
-		"x-rapidapi-key": "3200c9fad7mshfe59a992947dfa3p1a1ce7jsnea5b41ff8c0d",
-		"content-type": "application/x-www-form-urlencoded"
-	},
-	"data": {}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
-
 
 
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=cancer&day=today",
+	"url": "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=leo&?sign=cancer&?sign=sagittarius&?sign=libra&day=today",
 	"method": "POST",
 	"headers": {
 		"x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
-		"x-rapidapi-key": "3200c9fad7mshfe59a992947dfa3p1a1ce7jsnea5b41ff8c0d",
+		"x-rapidapi-key": "a5fda4c82emsh1f5d00eeb5ef450p1dba09jsndd9e8455fd50",
 		"content-type": "application/x-www-form-urlencoded"
 	},
 	"data": {}
 }
 
-$.ajax(settings).done(function (response) {
+= $.ajax(settings).done(function (response) {
 	console.log(response);
 });
 
-
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=cancer&day=today",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
-		"x-rapidapi-key": "3200c9fad7mshfe59a992947dfa3p1a1ce7jsnea5b41ff8c0d",
-		"content-type": "application/x-www-form-urlencoded"
-	},
-	"data": {}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-	var results = response.data;
-});
