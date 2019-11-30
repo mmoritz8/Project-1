9f4a1b2f290c554d67c970651670f30162a3e917
madeline moritz <mmoritz@stedwards.edu>
2019-11-29 23:28:55 -0600

    not much better with the images, but still working at it

--- Mads/js/article.js
+++ Mads/js/article.js
@@ -1,3 +1,6 @@
+var ima = $("<img>");
+ima.empty();
+
 $.ajax({
      url: 'https://newsapi.org/v2/everything?q=+local+news+austin+texas&from=2019-11-25&to=2019-11-25&sortBy=popularity&pageSize=1&apiKey=d9da22da0a2347a58fea3976369ee1f1',
      method: "GET",
@@ -10,9 +13,10 @@ $.ajax({
      var summary = document.getElementById("news-sum-2");
      $("#news-sum-2").text(response.articles[0].description);
 
-     var image = document.getElementById(".mov-img-p");
-
-    $(".mov-img-p").append("<img>").attr("src", response.articles[0].urlToImage); 
+     var image = document.getElementById("#news-img-2");
+     image.onload = function (e) {
+          $("#news-img-2").attr('src', e.target.result.articles[0].urlToImage)
+     }
 
           var summary = document.getElementById("news-url-2");
      $("#news-url-2").text(response.articles[0].url);
