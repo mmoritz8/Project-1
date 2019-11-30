24e96f4bb35d38c34ccf54edc173784ce8de8724
madeline moritz <mmoritz@stedwards.edu>
2019-11-30 12:58:21 -0600

    added the weather and a quote of the day

--- Mads/js/article.js
+++ Mads/js/article.js
@@ -7,22 +7,55 @@ $.ajax({
      var results = response.data;
      var title = document.getElementById("news-title-2");
      $("#news-title-2").text(response.articles[0].title);
-     var title = document.getElementById("news-title-5");
-     $("#news-title-2").text(response.articles[1].title);
 
 
      var summary = document.getElementById("news-sum-2");
      $("#news-sum-2").text(response.articles[0].description);
 
-     var summary = document.getElementById("news-sum-5");
-     $("#news-sum-5").text(response.articles[1].description);
 
-          var summary = document.getElementById("news-url-2");
+          var urls = document.getElementById("news-url-2");
      $("#news-url-2").text(response.articles[0].url);
-
-          var summary = document.getElementById("news-url-5");
-     $("#news-url-5").text(response.articles[1].url);
-     
 
      
 })
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lang=en&units=I&lat=30.2672&lon=-97.7431",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+		"x-rapidapi-key": "a5fda4c82emsh1f5d00eeb5ef450p1dba09jsndd9e8455fd50"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+     console.log(response);
+     var cityname = document.getElementById("com-title-3");
+     $("#com-title-3").text(response.city_name + response.state_code);
+     var weather = document.getElementById("com-sum-3");
+     $("#com-sum-3").text(response.data[9].temp + 'F');
+     
+});
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://150000-quotes.p.rapidapi.com/keyword/Life",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "150000-quotes.p.rapidapi.com",
+		"x-rapidapi-key": "a5fda4c82emsh1f5d00eeb5ef450p1dba09jsndd9e8455fd50"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+     console.log(response);
+     var message = document.getElementById("com-title-5");
+     $(message).empty();
+     $(message).text(response.message);
+     var author = document.getElementById("com-sum-5");
+     $(author).empty();
+     $(author).text(response.author);
+});
